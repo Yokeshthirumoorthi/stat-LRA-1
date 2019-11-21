071e48aadc64f2bed53f9ad7a1797219dafa39a9
Yokesh <yokeshthirumoorthi@gmail.com>
2019-11-21 01:21:04 -0800

    add 5.13 md

--- Q5.13.R
+++ Q5.13.R
@@ -2,23 +2,40 @@ library(readr)
 library(car)
 
 # Read data from csv
-data <- read_csv("TableCatapult.csv")
+data <- read_csv("TableCatapult_v1.csv")
 
-# Fit data to linear model
+# First order regression model fit
 model_mean <- lm(mean_y ~ X1 + X2 + X3 + X4, data = data)
 
 # Print the model summary
 summary(model_mean)
 
+# diagnostic plots of the linear regression fit
+plot(hist(resid(model_mean)))
 
 par(mfrow = c(2, 2))
 plot(model_mean)
 
+# Read data from csv
+data <- read_csv("TableCatapult_v2.csv")
+
 # Fit data to linear model
-model_var <- lm(square(var_y) ~ X1 + X2 + X3 + X4, data = data)
+model_sample_var <- lm(mean_y ~ X1 + X2 + X3 + X4, data = data, weights = data$weights)
 
 # Print the model summary
-summary(model_var)
+summary(model_sample_var)
+
+
+# Read data from csv
+data <- read_csv("TableCatapult_v2.csv")
+
+# Fit data to linear model
+model_est_var <- lm(log(var_y) ~ X1 + X2 + X3 + X4, data = data)
+
+# Print the model summary
+summary(model_est_var)
+
+data <- read_csv("TableCatapult_v3.csv")
 
 # Fit data to linear model
 model_weights <- lm(mean_y ~ X1 + X2 + X3 + X4, data = data, weights = data$weights)
